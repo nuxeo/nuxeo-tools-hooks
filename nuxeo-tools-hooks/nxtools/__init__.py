d276d0597742b6fc33d03ee5466da8ac3cf62ef2
Pierre-Gildas MILLON <pgmillon@nuxeo.com>
2016-05-16 12:38:57 +0200

    NXBT-1074: instanciate service on get rather than set

--- nuxeo-tools-hooks/nxtools/__init__.py
+++ nuxeo-tools-hooks/nxtools/__init__.py
@@ -1,29 +1,39 @@
+from types import ClassType
+
 
 class ServiceContainer(object):
 
-    def __init__(self):
-        self.__services = dict()
-
     @staticmethod
-    def service(name=None):
-        def decorator(f):
-            services.add(f(), name)
-            return f
-        return decorator
+    def service(clazz, name=None):
+        services.add(clazz, name)
+        return clazz
+
+    def __init__(self):
+        self.__raw = []
+        self.__values = []
 
     def get(self, clazz, name=None):
-        for service_name in self.__services:
-            service = self.__services[service_name]
-            if clazz == type(service):
-                if name:
-                    if name == service_name:
-                        return service
-                else:
-                    return service
-        raise KeyError
+        if name:
+            return [raw_value for raw_type, raw_name, raw_value in self.list(clazz) if raw_name == name][0]
+        else:
+            return [raw_value for raw_type, raw_name, raw_value in self.list(clazz)][0]
+
+    def list(self, clazz):
+
+        for raw_type, raw_name, raw_value in self.__raw:
+            if clazz == raw_type:
+                value = raw_type, raw_name, raw_value()
+                if value not in self.__values:
+                    self.__values.append(value)
+
+        return [(raw_type, raw_name, raw_value) for raw_type, raw_name, raw_value in self.__values if clazz == raw_type]
 
     def add(self, service, name=None):
-        name = name if name else type(service).__name__
-        self.__services[name] = service
+        if isinstance(service, type) or isinstance(service, ClassType):
+            name = name if name else service.__module__ + "." + service.__name__
+            self.__raw.append((service, name, lambda: service()))
+        else:
+            name = name if name else service.__module__ + "." + type(service).__name__
+            self.__raw.append((type(service), name, lambda: service))
 
 services = ServiceContainer()
