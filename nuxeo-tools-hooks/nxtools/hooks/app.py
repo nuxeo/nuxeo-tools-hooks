3617bd4acab4f429d30d7439f053ba35fcf62b7d
Pierre-Gildas MILLON <pgmillon@nuxeo.com>
2016-05-17 17:07:35 +0200

    NXBT-1074: running application

--- nuxeo-tools-hooks/nxtools/hooks/app.py
+++ nuxeo-tools-hooks/nxtools/hooks/app.py
@@ -1,29 +1,48 @@
 #!/usr/bin/env python
 
 import logging
-
 import os
 
 from flask.app import Flask
+from nxtools import services
 from nxtools.hooks import DEFAULTSECT
+from nxtools.hooks.endpoints.webhook import WebHookEndpoint
 from nxtools.hooks.services.config import Config
-
-config_file = os.getenv(Config.ENV_PREFIX + "CONF", os.getcwd() + "/conf/nuxeo-tools-hooks.ini")
-config = Config(config_file)
-
-log_file = config.get(DEFAULTSECT, "log_file")
-if not os.path.exists(os.path.dirname(log_file)):
-    os.makedirs(os.path.dirname(log_file))
-
-logging.basicConfig(
-    filename=log_file,
-    level=logging._levelNames[config.get(DEFAULTSECT, "log_level", "INFO").upper()])
+from nxtools.hooks.services.database import DatabaseService
 
 
-app = Flask(__name__)
+class ToolsHooksApp(object):
+
+    def __init__(self):
+        config_file = os.getenv(Config.ENV_PREFIX + "CONF", os.getcwd() + "/conf/nuxeo-tools-hooks.ini")
+        services.add(Config(config_file))
+
+    @property
+    def config(self):
+        return services.get(Config)
+
+    def run(self):
+
+        log_file = self.config.get(DEFAULTSECT, "log_file")
+        if log_file and not os.path.exists(os.path.dirname(log_file)):
+            os.makedirs(os.path.dirname(log_file))
+
+        logging.basicConfig(
+            filename=log_file,
+            level=logging._levelNames[self.config.get(DEFAULTSECT, "log_level", "INFO").upper()])
+
+        app = Flask(__name__)
+        services.get(DatabaseService).connect()
+
+        app.register_blueprint(WebHookEndpoint.blueprint(), url_prefix="/hook")
+
+        app.run(
+            host=self.config.get(DEFAULTSECT, "listen_address", "0.0.0.0"),
+            port=self.config.getint(DEFAULTSECT, "port", 8888),
+            debug=self.config.getboolean(DEFAULTSECT, "debug", False))
+
+
+app = ToolsHooksApp()
 
 if __name__ == '__main__':
-    app.run(
-        host=config.get(DEFAULTSECT, "listen_address", "0.0.0.0"),
-        port=config.getint(DEFAULTSECT, "port", 8888),
-        debug=config.getboolean(DEFAULTSECT, "debug", False))
+    app.run()
