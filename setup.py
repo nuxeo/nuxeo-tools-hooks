9f96f91a28c1e42da9e4786efbad1c294a3d0c57
Pierre-Gildas MILLON <pgmillon@nuxeo.com>
2016-07-21 16:05:33 +0200

    Preparing release 1.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_files(paths):
 
 setup(
     name='nuxeo-tools-hooks',
-    version='1.0.0.dev1',
+    version='1.0.0',
     license='ASL',
     author='Nuxeo',
     author_email='contact@nuxeo.com',
