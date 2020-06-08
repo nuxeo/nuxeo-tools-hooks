716c5a19c74c0b478dd572dce1bc357f108aada3
Anahide Tchertchian <at@nuxeo.com>
2020-06-08 15:48:24 +0200

    RELEASE-1005: release version 1.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_files(paths):
 
 setup(
     name='nuxeo-tools-hooks',
-    version='1.1.2.dev0',
+    version='1.1.2',
     license='ASL',
     author='Nuxeo',
     author_email='contact@nuxeo.com',
